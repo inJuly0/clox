18e75a4d6a59c7b1d9636d976883021e294aceaa
tiltproofRain <srijannr1@gmail.com>
2020-09-30 18:39:47 +0530

    ran clang-format on all files

--- src/compiler.h
+++ src/compiler.h
@@ -4,7 +4,7 @@
 #include "chunk.h"
 #include "object.h"
 
-ObjFunction* compile(const char* source);
-void printTokens(const char* source);
+ObjFunction *compile(const char *source);
+void printTokens(const char *source);
 
 #endif
